4c853f0a4e16f558e1b9cab7c6c6c149e2b2d0f7
Abiatou <abiatoubadji5@gmail.com>
2026-03-15 10:18:12 +0100

    Initial commit - TodoApi

---
+++ Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+using TodoApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
